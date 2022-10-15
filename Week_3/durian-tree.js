16be08e971a80eb000d9c732a314d13c725e27b1
Anit <anittankandasamy@gmail.com>
2022-10-15 03:41:03 +0000

    Week 3 notes on DOM OOP

--- Week_3/durian-tree.js
+++ Week_3/durian-tree.js
@@ -8,7 +8,7 @@ class Employee {
   }
 
   addSubordinate(subordinate) {
-    this.subordinates.push(subordinate)
+    this.subordinates.push(subordinate);
     subordinate.boss = this;
   }
 
@@ -20,7 +20,7 @@ class Employee {
     let numberOfPeople = 0;
     let currentEmployee = this;
 
-    while(currentEmployee.boss) {
+    while (currentEmployee.boss) {
       currentEmployee = currentEmployee.boss;
       numberOfPeople++;
     }
@@ -29,8 +29,36 @@ class Employee {
   }
 
   hasSameBoss(employee) {
-    return this.boss === employee.boss
+    return this.boss === employee.boss;
   }
+
+  employeesThatMakeOver(amount) {
+
+    //Create a new employees array to hold every employee that makes over the specified amoun
+    let employees = []; // 1
+
+    // If the current employee makes over that amount, add them to the array.
+    if (this.salary > amount) {
+      employees.push(this); // 2
+    }
+    
+    //Call this method on all of the current employee's subordinates and combine their results with the current results.
+    for (const subordinate of this.subordinates) {
+      const subordinatesThatMakeOver = subordinate.employeesThatMakeOver(amount); // 3
+      employees = employees.concat(subordinatesThatMakeOver);
+    }
+
+    return employees;
+  }
+
+  get totalEmployees() {
+    let totalEmployees = 1;
+    for (const subordinate of this.subordinates) {
+      totalEmployees += subordinate.totalEmployees;
+    }
+    return totalEmployees;
+  }
+
 }
 
 //CEO
@@ -42,12 +70,12 @@ const arvinder = new Employee("Arvinder", "Chief Design Officer", 1000000);
 const angela   = new Employee("Angela", "VP Retail", 1000000);
 const phil     = new Employee("Phil", "VP Marketing", 1000000);
 
-const simone = new Employee("Simone", "Employee", 70000)
-const ali = new Employee("Ali", "Employee", 70000)
-const florida = new Employee("Florida", "Employee", 40000)
-const david = new Employee("Davis", "Employee", 50000)
-const brian = new Employee("Brian", "Employee", 40000)
-const karla = new Employee("Karla", "Employee", 30000)
+const simone = new Employee("Simone", "Employee", 70000);
+const ali = new Employee("Ali", "Employee", 70000);
+const florida = new Employee("Florida", "Employee", 40000);
+const david = new Employee("Davis", "Employee", 50000);
+const brian = new Employee("Brian", "Employee", 40000);
+const karla = new Employee("Karla", "Employee", 30000);
 
 ada.addSubordinate(craig);
 ada.addSubordinate(arvinder);
@@ -67,4 +95,6 @@ angela.addSubordinate(karla);
 
 // console.log(craig.boss);
 // console.log(craig.numberOfSubordinates);
-console.log(craig.numberOfPeopleToCEO);
+// console.log(craig.numberOfPeopleToCEO);
+console.log(ada.totalEmployees);
+
